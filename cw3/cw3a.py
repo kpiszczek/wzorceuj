775a89e0b14b17363181bc205f3318c726886c7b
kpiszczek <jppiszczek@gmail.com>
2013-04-10 14:43:40 +0200

    ćwiczenie 3

--- cw3/cw3a.py
+++ cw3/cw3a.py
@@ -1,44 +1,148 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import abc
+from tkinter import Tk, Frame, Label, Button
 
-class FormBuilder:
+class Form(object):
+    """
+    Produkt
+    """
+    def __init__(self):
+        self.message = None
+        self.buttons = None
+
+    def __str__(self):
+        return 'message: "%s", buttons: %s' % (self.message,self.buttons)
+
+
+class FormBuilder(object):
+    """
+    Kierownik
+    """
+    def __init__(self):
+        self.but_builder = None
+        self.wm_builder = None
+        
     def constructForm(self):
-        pass
+        # tworzymy formularz tylko wtedy gdy istnieją budowniczowie
+        if self.but_builder is not None and self.wm_builder is not None:
+            self.but_builder.new_form()
+            self.but_builder.addButtons()
+            self.wm_builder.form = self.but_builder.form
+            self.wm_builder.printWM()
 
-class ButtonsBuilder(metaclass=abc.ABCMeta):
+    def setButtonsBuilder(self,but_builder):
+        self.but_builder = but_builder
+
+    def setWMBuilder(self,wm_builder):
+        self.wm_builder = wm_builder
+
+    def getForm(self):
+        return self.wm_builder.form
+
+
+class Builder(object):
+    """
+    Budowniczy
+    """
+    def __init__(self):
+        self.form = None
+
+    def new_form(self):
+        self.form = Form()
+
+    
+class ButtonsBuilder(Builder,metaclass=abc.ABCMeta):
+    """
+    Budowniczy
+    """
     @abc.abstractmethod
     def addButtons(self):
         pass
 
+
 class StudentsButtonsBuilder(ButtonsBuilder):
+    """
+    Budowniczy konkretny
+    """
     def addButtons(self):
-        pass
+        self.form.buttons = ('Aktualności','Plan zajęć', 'Oceny')
+
 
 class AdminButtonsBuilder(ButtonsBuilder):
+    """
+    Budowniczy konkretny
+    """
     def addButtons(self):
-        pass
+        self.form.buttons = ('Aktualności', 'Użytkownicy', 'Prowadzący')
+
 
 ButtonsBuilder.register(StudentsButtonsBuilder)
 ButtonsBuilder.register(AdminButtonsBuilder)
 
+
 class WelcomeMessageBuilder(metaclass=abc.ABCMeta):
+    """
+    Budowniczy
+    """
     @abc.abstractmethod
     def printWM(self):
         pass
+        
 
 class StudentsWMBuilder(WelcomeMessageBuilder):
+    """
+    Budowniczy konkretny
+    """
     def printWM(self):
-        pass
+        self.form.message = "Witaj! Już po sesji?"
+
 
 class AdminWMBuilder(WelcomeMessageBuilder):
+    """
+    Budowniczy konkretny
+    """
     def printWM(self):
-        pass
+        self.form.message = "Hej! Na co się dzisiaj skarżą użyszkodnicy?"
+
 
 WelcomeMessageBuilder.register(StudentsWMBuilder)
 WelcomeMessageBuilder.register(AdminWMBuilder)
 
-def test():
-    pass
 
+class Window(Frame):
+    """
+    Klasa pomocnicza do wyświetlenia formularza
+    """
+    def __init__(self,parent,form):
+        super(Window,self).__init__(parent)
+        self.parent = parent
+        self.message = Label(self,text=form.message)
+        self.message.pack(padx=10,pady=10)
+        self.buttons = [Button(self,text=b) for b in form.buttons]
+        for button in self.buttons:
+            button.pack(side="left", padx=10,pady=10)            
+        self.pack()
+
+
+def test():
+    form_builder = FormBuilder()
+    but_builder = StudentsButtonsBuilder()
+    wm_builder = StudentsWMBuilder()
+
+    form_builder.setButtonsBuilder(but_builder)
+    form_builder.setWMBuilder(wm_builder)
+
+    form_builder.constructForm()
+    
+    form = form_builder.getForm()
+
+    # tworzenie okna programu    
+    root = Tk()
+    w = Window(root,form)
+    root.geometry("400x100+100+100")
+    root.mainloop()
 
 if __name__ == "__main__":
     test()
---
+++ cw3/cw3b.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import abc
+import copy
+from random import random
+
+
+class Multimedia(metaclass=abc.ABCMeta):
+    """
+    Prototyp
+    """
+    def __init__(self,name,type_,data=None):
+        self.name = name
+        self.type_ = type_
+        # jeśli nie zostały przesłane dane początkowe to
+        # wypełniamy obiekt losowymi danymi
+        if data is None:
+            self.populate_data()
+        else:
+            self.data = data
+    
+    @abc.abstractmethod
+    def clone(self):
+        return copy.deepcopy(self)
+
+    @abc.abstractmethod
+    def toString(self):
+        return "%s: %s" % (self.type_,self.name)
+
+    @abc.abstractmethod
+    def populate_data(self):
+        pass
+
+    def __eq__(self,other):
+        """
+        Definicja zachowania dla operatora ==
+        Pozwala na sprawdzenie czy zawartość danych
+        w dwóch obiektach jest identyczna
+        """
+        return (self.name == other.name) and \
+               (self.type_ == other.type_) and \
+               (self.data == other.data)
+
+
+class Picture(Multimedia):
+    """
+    Prototyp konkretny
+    """
+    def __init__(self,name="new_picture",data=None):
+        # wywołanie konstruktora klasy bazowej
+        super(Picture,self).__init__(name,"Picture",data)
+
+    def clone(self):
+        # wywołanie metody klasy bazowej
+        return super(Picture,self).clone()
+
+    def toString(self):
+        # wywołanie metody klasy bazowej
+        return super(Picture,self).toString()
+
+    def populate_data(self):
+        # tworzymy dwuwymiarową tablicę losowych
+        # trójelementowych krotek (RGB) o rozmiarze 5x5
+        self.data = [[(random(),random(),random())
+                      for _ in range(5)]
+                     for _ in range(5)]
+
+
+class Music(Multimedia):
+    """
+    Prototyp konkretny
+    """
+    def __init__(self,name="new_music",data=None):
+        # wywołanie konstruktora klasy bazowej
+        super(Music,self).__init__(name,"Music",data)
+
+    def clone(self):
+        # wywołanie metody klasy bazowej
+        return super(Music,self).clone()
+
+    def toString(self):
+        # wywołanie metody klasy bazowej
+        return super(Music,self).toString()
+
+    def populate_data(self):
+        # tworzymy 256-elementową tablicę losowych wartości (próbek)
+        self.data = [random() for _ in range(256)]       
+        
+
+class Movie(Multimedia):
+    """
+    Prototyp konkretny
+    """ 
+    def __init__(self,name="new_movie",data=None):
+        # wywołanie konstruktora klasy bazowej
+        super(Movie,self).__init__(name,"Movie",data)
+
+    def clone(self):
+        # wywołanie metody klasy bazowej
+        return super(Movie,self).clone()
+
+    def toString(self):
+        # wywołanie metody klasy bazowej
+        return super(Movie,self).toString()
+
+    def populate_data(self):
+        # tworzymy tablicę 25 obiektów klasy Picture
+        self.data = [Picture(str(i)) for i in range(25)]
+    
+
+# zapewnienie, że wszytkie obiekty klas Picture, Music oraz Movie
+# implementują wszystkie metody abstrakcyjne klasy Multimedia
+Multimedia.register(Picture)
+Multimedia.register(Music)
+Multimedia.register(Movie)
+
+
+def test():
+    # tworzymy listę zawierającą po jednym obiekcie każdej klasy konkretnej
+    my_multimedia = [Picture(),Music(),Movie()]
+
+    # tworzymy listę 'klonów' elementów listy my_multimedia
+    multimedia_copy = [mm.clone() for mm in my_multimedia]
+
+    # iterujemny równolegle po elementach obu list
+    # mc - element listy 'klonów', mm - element listy 'oryginałów'
+    for mc, mm in zip(multimedia_copy, my_multimedia):
+        print("Obiekt z listy 'my_multimedia':\t\t",mm.toString())
+        print("Obiekt z listy 'multimedia_copy':\t",mc.toString())
+        print("Zawartość danych jest identyczna:\t", mm.data == mc.data)
+        print("Kolekcja danych to ten sam obiekt:\t", mm.data is mc.data,"\n")
+
+if __name__ == "__main__":
+    test()
