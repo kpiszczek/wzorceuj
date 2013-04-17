756dcdefdf6e46639b37e73ba424fd823a98746e
kpiszczek <jppiszczek@gmail.com>
2013-04-17 18:44:07 +0200

    cwiczenie 4

---
+++ cw4/cw4a.py
@@ -0,0 +1,183 @@
+import abc
+import datetime
+from collections import namedtuple
+
+# struktura do przechowywania danych o dacie
+DateISO = namedtuple("Date",["year","month","day"])
+DateUSA = namedtuple("Date",["year","day","month"])
+
+class DataFormat(metaclass=abc.ABCMeta):
+    """
+    Implementor
+    """
+    def __init__(self, date=None, filename=None):
+        self.date = date
+        self.filename = filename
+
+    @abc.abstractmethod
+    def write(self,date):
+        pass
+
+    def setDate(self, date):
+        self.date = date
+
+    def setFile(self, filename):
+        self.filename = filename
+
+
+class CSVData(DataFormat):
+    """
+    Concrete implementor
+    """
+    def write(self,date=None):
+        if date is not None:
+            self.date = date
+        with open(self.filename,"w+") as file:
+            d = ",".join(map(str,self.date))
+            print(d)
+            file.write(d)
+
+
+class XMLData(DataFormat):
+    """
+    Concrete implementor
+    """
+    def write(self,date=None):
+        if date is not None:
+            self.date = date
+        with open(self.filename, "w+") as file:
+            d = "<date>"+"-".join(map(str,self.date))+"</date>"
+            print(d)
+            file.write(d)
+
+
+class FixedLenData(DataFormat):
+    """
+    Concrete implementor
+    """
+    def write(self,date=None):
+        if date is not None:
+            self.date = date
+        with open(self.filename, "w+") as file:
+            d = "".join(map(str,self.date)).rjust(8,"-")
+            print(d)
+            file.write(d)
+
+
+DataFormat.register(CSVData)
+DataFormat.register(XMLData)
+DataFormat.register(FixedLenData)
+
+
+class State(metaclass=abc.ABCMeta):
+    """
+    State
+    """
+    @abc.abstractmethod
+    def toggle(self):
+        pass
+
+    @abc.abstractmethod
+    def getDate(self):
+        pass
+
+
+class CompactState(State):
+    """
+    Concrete state
+
+    Rok zapisywany za pomocą dwóch cyfr
+    """   
+    def toggle(self):
+        return FullState()
+
+    def getDate(self):
+        d = datetime.date.today()
+        return DateISO(year=str(d.year)[2:],
+                       month=str(d.month).zfill(2),
+                       day=str(d.day).zfill(2))
+
+
+class FullState(State):
+    """
+    Concrete state
+
+    Rok zapisywany za pomocą czterech cyfr
+    """
+    def toggle(self):
+        return CompactState()
+        
+    def getDate(self):
+        d = datetime.date.today()
+        return DateISO(year=str(d.year),
+                       month=str(d.month).zfill(2),
+                       day=str(d.day).zfill(2))
+
+
+State.register(CompactState)
+State.register(FullState)
+
+
+class OutputFile(object):
+    """
+    Abstraction
+
+    Tworzy w konstruktorze jeden, odpowiedni obiekt implementacji
+
+    Przechowuje dane o stanie (trybie) zapisu: krótki/pełny
+    """
+    def __init__(self,filename,format_="csv"):
+        self.filename = filename
+        self.__state = FullState()
+        if format_ == "csv":
+            self.__format = CSVData(filename=self.filename)
+        elif format_ == "xml":
+            self.__format = XMLData(filename=self.filename)
+        elif format_ == "fixedLen":
+            self.__format = FixedLenData(filename=self.filename)
+    
+    def writeUSADateFormat(self):
+        d = self.__state.getDate()
+        d = DateUSA(year=d.year,
+                    day=d.day,
+                    month=d.month)
+        self.__format.write(d)
+
+    def writeISODateFormat(self):
+        d = self.__state.getDate()
+        d = DateISO(year=d.year,
+                    day=d.day,
+                    month=d.month)
+        self.__format.write(d)
+
+    def toggleCompact(self):
+        self.__state = self.__state.toggle()
+        
+
+
+def test():
+    out = OutputFile("date.csv",format_="csv")
+    out.writeUSADateFormat()
+    out.writeISODateFormat()
+    #zmieniamy tryb na krótki (rok zapisywany dwiema cyframi)
+    out.toggleCompact()
+    out.writeUSADateFormat()
+    out.writeISODateFormat()
+    
+    out = OutputFile("date.xml",format_="xml")
+    out.writeUSADateFormat()
+    out.writeISODateFormat()
+    out.toggleCompact()
+    out.writeUSADateFormat()
+    out.writeISODateFormat()
+
+    out = OutputFile("date.txt",format_="fixedLen")
+    out.writeUSADateFormat()
+    out.writeISODateFormat()
+    out.toggleCompact()
+    out.writeUSADateFormat()
+    out.writeISODateFormat()
+    
+if __name__ == "__main__":
+    test()
+
---
+++ cw4/cw4b.py
@@ -0,0 +1,95 @@
+import types
+from copy import copy
+from random import randrange, randint
+
+# Ze względu na dynamiczną naturę języka Python nie ma konieczności
+# tworzenie klas strategii konkretnych. Zamiast tego można dynamicznie
+# zmienić (przypisać) metodę do istniejącego obiektu
+
+class Sorter(object):
+    """
+    Strategia
+    """
+    def __init__(self,algo=None):
+        if algo:
+            # konwersja funkcji na metodę
+            # dzięki temu dynamicznie przypisana procedura będzie
+            # miała dostęp do atrybutów klasy (będzie do niej przesyłany
+            # argument 'self'
+            self.algo = types.MethodType(algo, self)
+
+    def sort(self,iterable):
+        if self.algo is not None:
+            return self.algo(copy(iterable))
+        else:
+            raise NotImplemented
+
+    
+def shellSort(self, array):
+     # // - dzielenie całkowitoliczbowe
+    gap = len(array) // 2
+    while gap > 0:
+        for i in range(gap, len(array)):
+            val = array[i]
+            j = i
+            while j >= gap and array[j - gap] > val:
+                array[j] = array[j - gap]
+                j -= gap
+            array[j] = val
+        gap //= 2
+    return array
+
+def heapSort(self, A):
+    
+    def heapify(A):
+        start = (len(A) - 2) // 2
+        while start >= 0:
+            siftDown(A, start, len(A) - 1)
+            start -= 1
+
+    def siftDown(A, start, end):
+        root = start
+        while root * 2 + 1 <= end:
+            child = root * 2 + 1
+            if child + 1 <= end and A[child] < A[child + 1]:
+                child += 1
+            if child <= end and A[root] < A[child]:
+                A[root], A[child] = A[child], A[root]
+                root = child
+            else:
+                return
+            
+    heapify(A)
+    end = len(A) - 1
+    while end > 0:
+        A[end], A[0] = A[0], A[end]
+        siftDown(A, 0, end - 1)
+        end -= 1
+    return A
+
+def quickSort(self,array):
+    def qsort(array):
+        if array == []: 
+            return []
+        else:
+            pivot = array.pop(randrange(len(array)))
+            lesser = qsort([l for l in array if l < pivot])
+            greater = qsort([l for l in array if l >= pivot])
+            return lesser + [pivot] + greater
+    return qsort(array[:])
+
+def test():
+    # lista 10 losowych liczba całkowitych
+    array = [randint(0,10) for _ in range(10)]
+    
+    sorter = Sorter(shellSort)
+    print(sorter.sort(array))
+    
+    sorter = Sorter(quickSort)
+    print(sorter.sort(array))
+    
+    sorter = Sorter(heapSort)
+    print(sorter.sort(array))
+
+if __name__ == "__main__":
+    test()
